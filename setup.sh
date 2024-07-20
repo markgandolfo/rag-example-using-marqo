2903bf826cadd24a39a1c6ee0fe19c2acdb7899f
Mark <mark@gandolfo.com.au>
2024-07-20 14:04:22 +1000

    run marqo docker as a daemon

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo 'setting up Marqo docker'
 docker pull marqoai/marqo:latest
 docker rm -f marqo
-docker run --name marqo -it -p 8882:8882 marqoai/marqo:latest
+docker run --name marqo -d -p 8882:8882 marqoai/marqo:latest
 
 echo 'Setting up your environment...'
 python3 -m venv .venv
